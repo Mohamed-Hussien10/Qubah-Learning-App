5e285dd9f2f4ee1b77dbcdfe653715b34921b362
Mohamed Hussien <mohamedhu350@gmail.com>
2026-09-16 07:21:28 +0300

    fix app run issues

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -50,8 +50,15 @@ android {
     buildTypes {
         release {
             signingConfig = signingConfigs.getByName("release")
-            isMinifyEnabled = false
-            isShrinkResources = false
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+            ndk {
+                debugSymbolLevel = "SYMBOL_TABLE"
+            }
         }
         debug {
             signingConfig = signingConfigs.getByName("debug")
---
+++ android/app/proguard-rules.pro
@@ -0,0 +1,9 @@
+# Flutter wrapper
+-keep class io.flutter.app.** { *; }
+-keep class io.flutter.plugin.**  { *; }
+-keep class io.flutter.util.**  { *; }
+-keep class io.flutter.view.**  { *; }
+-keep class io.flutter.**  { *; }
+-keep class plugins.flutter.io.**  { *; }
+-dontwarn io.flutter.plugin.**
+-dontwarn com.google.android.play.core.**
